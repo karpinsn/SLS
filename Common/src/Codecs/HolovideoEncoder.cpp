2337b4504b53028871844495e021e50e8df36456
Nik Karpinsky <karpinsn@iastate.edu>
2012-01-11 14:47:34 -0600

    Got the HolovideoEncoder using Reactor to output videos. Some work needs to be done to convert between image formats

--- Common/src/Codecs/HolovideoEncoder.cpp
+++ Common/src/Codecs/HolovideoEncoder.cpp
@@ -3,6 +3,29 @@
 HolovideoEncoder::HolovideoEncoder()
 {
   m_filename  = "";
+  m_image = cvCreateImage(cvSize(512, 512), IPL_DEPTH_8U, 3);
+  m_yuv444toyuv422.init(PIX_FMT_YUV444P, PIX_FMT_YUV422P); 
+
+  yuv444Frame = avcodec_alloc_frame();
+  yuv422Frame = avcodec_alloc_frame();
+
+  yuv444Frame->width = 512;
+  yuv444Frame->height = 512;
+  yuv422Frame->width = 512;
+  yuv422Frame->height = 512;
+
+  int numBytes = avpicture_get_size(PIX_FMT_YUV444P, 512, 512);
+  yuv444Buffer = (uint8_t*)av_malloc(numBytes * sizeof(uint8_t));
+  avpicture_fill((AVPicture*)yuv444Frame, yuv444Buffer, PIX_FMT_YUV444P, 512, 512);
+
+  numBytes = avpicture_get_size(PIX_FMT_YUV422P, 512, 512);
+  yuv422Buffer = (uint8_t*)av_malloc(numBytes * sizeof(uint8_t));
+  avpicture_fill((AVPicture*)yuv422Frame, yuv422Buffer, PIX_FMT_YUV422P, 512, 512);
+}
+
+HolovideoEncoder::~HolovideoEncoder()
+{
+  cvReleaseImage(&m_image);
 }
 
 void HolovideoEncoder::initCodec(string& filename, int width, int height)
@@ -26,13 +49,18 @@ void HolovideoEncoder::openCodec(EncodingOpenGLWidget* glWidget)
   m_glWidget->setGLContext(&m_encoder);
   m_glWidget->reinit(getWidth(), getHeight());
 
-  m_io.openSaveStream(m_filename, getWidth(), getHeight(), 30);
+  if(!m_filename.empty())
+  {
+	m_videoWriter.openFile(m_filename);
+  }
+  //m_io.openSaveStream(m_filename, getWidth(), getHeight(), 30);
 }
 
 void HolovideoEncoder::closeCodec(void)
 {
   //  Close the stream
-  m_io.closeSaveStream();
+  m_videoWriter.closeFile();
+  //m_io.closeSaveStream();
 }
 
 void HolovideoEncoder::process(MeshInterchange* data)
@@ -46,9 +74,14 @@ void HolovideoEncoder::process(MeshInterchange* data)
   m_encoder.setCurrentMesh(data);
 
   MeshInterchange* mesh = m_glWidget->encode();
+  mesh->getTexture()->transferFromTexture(m_image);
+  
+  m_yuv444toyuv422.iplImage2AVFrame(m_image, yuv444Frame);
+  m_yuv444toyuv422.convert(reactor::VideoFrame(yuv444Frame, PIX_FMT_YUV444P), reactor::VideoFrame(yuv422Frame, PIX_FMT_YUV422P));
 
   //  Encode to the stream
-  m_io.saveStream(*(mesh->getTexture()));
+  m_videoWriter.writeFrame(reactor::VideoFrame(yuv422Frame, PIX_FMT_YUV422P));
+  //m_io.saveStream(*(mesh->getTexture()));
 }
 
 void HolovideoEncoder::previewProcess(MeshInterchange* data)
--- Common/src/Codecs/HolovideoEncoder.h
+++ Common/src/Codecs/HolovideoEncoder.h
@@ -10,19 +10,44 @@
 #include "Holodecoder.h"
 #include "Holoencoder.h"
 
+#include <reactor/VideoFileWriter.h>
+#include <Reactor/FormatConverter.h>
+#include <Reactor/VideoFrame.h>
+
+//	FFmpeg Includes
+#define __STDC_CONSTANT_MACROS
+extern "C"
+{
+  #include <libavcodec/avcodec.h>
+  #include <libavformat/avformat.h>
+  #include <libswscale/swscale.h>
+}
+
 using namespace std;
 
 class HolovideoEncoder : public Codec
 {
 private:
   VideoIO m_io;
+  reactor::VideoFileWriter m_videoWriter;
+  reactor::FormatConverter m_yuv444toyuv422;
+
+  AVFrame* yuv444Frame;
+  AVFrame* yuv422Frame;
+  uint8_t* yuv444Buffer;
+  uint8_t* yuv422Buffer;
+
+
   Holoencoder m_encoder;
   EncodingOpenGLWidget* m_glWidget;
 
   string m_filename;
 
+  IplImage* m_image;
+
 public:
   HolovideoEncoder();
+  ~HolovideoEncoder();
   void initCodec(string& filename, int width, int height);
 
   void openCodec(EncodingOpenGLWidget* glWidget);

20417debf86e7f2a0f276c0f64571f1441687658
Nik Karpinsky <karpinsn@iastate.edu>
2012-02-09 17:02:43 -0600

    Working on getting 3D manipulators working correctly

--- Common/src/Holodecoder.cpp
+++ Common/src/Holodecoder.cpp
@@ -21,7 +21,7 @@ void Holodecoder::init(float width, float height)
 	_initLighting();
 
 	m_controller.init(0.0, 0.0, -1.0, 0.2f);
-	m_camera.init(0.0f, 0.0f, 4.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+	m_camera.init(0.0f, 0.0f, 6.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
 	m_camera.setMode(1);
 
 	m_mesh = new TriMesh(512, 512);
